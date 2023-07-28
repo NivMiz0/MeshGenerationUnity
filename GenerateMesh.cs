ff400c91661d199221e2032133d66ac9157e3df1
Tom Waisberg <68729153+TomWaisberg@users.noreply.github.com>
2023-07-28 04:09:54 +0300

    Add files via upload

---
+++ GenerateMesh.cs
@@ -0,0 +1,208 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMesh : MonoBehaviour
+{
+    public SphereScriptableObject data;
+    public float vertexGizmoRad;
+
+    private void Update()
+    {
+        GenerateSphere();
+    }
+
+    //Generate the mesh for the asteroid based on data from the scriptableobject
+    void GenerateCircle()
+    {
+        Mesh mesh = new Mesh();
+        
+        mesh.Clear();
+        
+        List<Vector3> vertices = new List<Vector3>();
+        vertices.Add(new Vector3(0, data.radius));
+        vertices.Add(new Vector3(data.radius, 0));
+
+        //functions for a circle is r^2 = x^2 + y^2
+        //my algorithm for creating a circle with evenly placed vertices: we will start with a quarter of the circle - 
+        //start with only the two vertices at the intersection points with the axes. then, based on a resolution variable, 
+        //create N points on the straight line between the two vertices you already have (the line
+        //represented by the function y = -x + r), then transform (with some 
+        //analytic geometry) each of the points to the corresponding point on the circle. That point will be the vertex.
+        //Then simply copy the quarter four horizontally then vertically
+        
+        float step = Mathf.Sqrt(data.radius) / (data.resolution+1);
+        for (int i = 1; i <= data.resolution; i++)
+        {
+            float x = step*i;
+            float y = Mathf.Sqrt(data.radius) - x;
+
+            Vector3 newVert = ProjectOntoCircle(x, y);
+            vertices.Add(newVert);
+        }
+
+        //Mirror the quarter circle on the x axis
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 mirror = MirrorX(vertices[i]);
+            if (!vertices.Contains(mirror))
+            {
+                vertices.Add(mirror);
+            }
+        }
+        
+        //Mirror the quarter circle on the y axis
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 mirror = MirrorY(vertices[i]);
+            if (!vertices.Contains(mirror))
+            {
+                vertices.Add(mirror);
+            }
+        }
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = vertices.ToArray();
+    }
+
+    void GenerateSphere()
+    {
+        Mesh mesh = new Mesh();
+        
+        mesh.Clear();
+        
+        List<Vector3> vertices = new List<Vector3>();
+        vertices.Add(new Vector3(0, data.radius, 0));
+        vertices.Add(new Vector3(data.radius, 0, 0));
+        vertices.Add(new Vector3(0, 0, data.radius));
+
+        //functions for a sphere is r^2 = x^2 + y^2 + z^2 (centered at the origin, r being the radius)
+        //my algorithm for creating a sphere will use the same concepts I put into place on the circle:
+        //start with three points: one on each intersection point of your desired sphere with the three axes.
+        //focus only on one quadrant, in this case the first. Find a plane that bisects the sphere, that also intersects 
+        //with the three starting points, namely the plane represented by the function z = - y - x + r. 
+        //Now, if the resolution divided by three has a remainder of 1, create a point in the middle of the three starting 
+        //points on the triangle. the coordinates of this point will be (r/3, r/3, r/3). Now draw a line from this mid-point to each of 
+        //your starting points, and the length of these lines will be (r*sqrt(2)*sqrt(3))/3 (solved with analytic geometry).
+        //Use this distance, along with the desired resolution, to calculate the "step" - the length of each segment between
+        //vertices. place points along that line according to this "step", then project all resulting points on your plane 
+        //to the sphere, using the slope of the line function  between the origin and each point,
+        //then finding that line's intersection with the sphere function. Use the resulting projected points as your vertices.
+
+        Vector3 midPoint = new Vector3(data.radius / 3, data.radius / 3, data.radius / 3);
+        vertices.Add(ProjectOntoSphere(midPoint.x, midPoint.y, midPoint.z));
+
+        float sqrt2 = 1.4142135f;
+        float sqrt3 = 1.7320508f;
+        float centerDist = (data.radius * sqrt2 * sqrt3) / 3;
+        float step = centerDist / (data.resolution + 1);
+
+        //The points that point upward
+        for (int i = 1; i <= data.resolution/3; i++)
+        {
+            float x = step * Mathf.Pow(i, 1.1f);
+            float y = (data.radius - x) / 2; //z and x are equal in this
+            vertices.Add(ProjectOntoSphere(x, y, y));
+            vertices.Add(ProjectOntoSphere(y, x, y));
+            vertices.Add(ProjectOntoSphere(y, y, x));
+            
+            vertices.Add(ProjectOntoSphere(y, x, x));
+            vertices.Add(ProjectOntoSphere(x, x, y));
+            vertices.Add(ProjectOntoSphere(x, y, x));
+        }
+
+        //Mirror the eighth-sphere on the x axis
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 mirror = MirrorX(vertices[i]);
+            if (!vertices.Contains(mirror))
+            {
+                vertices.Add(mirror);
+            }
+        }
+        
+        //Mirror the eighth-sphere on the y axis
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 mirror = MirrorY(vertices[i]);
+            if (!vertices.Contains(mirror))
+            {
+                vertices.Add(mirror);
+            }
+        }
+        
+        //Mirror the eighth-sphere on the z axis
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 mirror = MirrorZ(vertices[i]);
+            if (!vertices.Contains(mirror))
+            {
+                vertices.Add(mirror);
+            }
+        }
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = vertices.ToArray();
+    }
+
+    //Analytic Geometry transformation code from line to circle
+    public Vector3 ProjectOntoCircle(float x, float y)
+    {
+        float slope = y / x;
+        
+        float newX = Mathf.Sqrt(Mathf.Pow(data.radius, 2) / (Mathf.Pow(slope, 2) + 1));
+        float newY = Mathf.Sqrt(Mathf.Pow(data.radius, 2) - Mathf.Pow(newX, 2));
+        Vector3 final = new Vector3(newX, newY);
+        return final;
+    }
+
+    //Analytic Geometry projection code from inner plane to sphere
+    public Vector3 ProjectOntoSphere(float x, float y, float z)
+    {
+        //the two slopes: m is for x and n is for z.
+        //the way I structured 3D linear functions is - 
+        //I: y = mx + nz + b
+        //II: y = mx + nz + b
+        //formula for finding slopes is the same as in 2D for m and swap the x's for z's for n
+        float m = y / x;
+        float n = y / z;
+
+        float newX = (data.radius * n) /
+                     Mathf.Sqrt(Mathf.Pow(n, 2) + Mathf.Pow(n, 2) * Mathf.Pow(m, 2) + Mathf.Pow(m, 2));
+        float newY = (data.radius * n * m) /
+                     Mathf.Sqrt(Mathf.Pow(n, 2) + Mathf.Pow(n, 2) * Mathf.Pow(m, 2) + Mathf.Pow(m, 2));
+        float newZ = (data.radius * m) /
+                     Mathf.Sqrt(Mathf.Pow(n, 2) + Mathf.Pow(n, 2) * Mathf.Pow(m, 2) + Mathf.Pow(m, 2));
+
+        return new Vector3(newX, newY, newZ);
+    }
+
+    public Vector3 MirrorX(Vector3 vector)
+    {
+        return new Vector3(-vector.x, vector.y, vector.z);
+    }
+
+    public Vector3 MirrorY(Vector3 vector)
+    {
+        return new Vector3(vector.x, -vector.y, vector.z);
+    }
+    
+    public Vector3 MirrorZ(Vector3 vector)
+    {
+        return new Vector3(vector.x, vector.y, -vector.z);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (GetComponent<MeshFilter>().mesh == null) return;
+        
+        Gizmos.color = Color.black; 
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        
+        foreach (Vector3 vertex in mesh.vertices) {
+            Vector3 worldPos = transform.TransformPoint(vertex);
+            Gizmos.DrawSphere(worldPos, vertexGizmoRad);
+        }
+    }
+}
